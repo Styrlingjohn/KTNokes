28c8bf7560f82381182296647e427374ecbc7cf6
Styrlingjohn <styrling_morris@yahoo.com>
2023-12-28 17:14:24 -0700

    added all pcitures and changed styling of carousel and icons

--- src/components/adds/Slider.js
+++ src/components/adds/Slider.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import Basement1 from './Pics/Basement1.jpg'
-import Basement2 from './Pics/Basement2.jpg'
+import Basement1 from './Pics/Basement1.jpg';
+import Basement2 from './Pics/Basement2.jpg';
+
+import F2 from './Pics/F2.jpg';
+
 
 
 
@@ -9,10 +12,11 @@ const Slider = () => {
     return (
         <div className='carousel'>
 
-            <Carousel fade>
+            <Carousel fade data-bs-theme='dark'> 
                 <Carousel.Item interval={5000}>
                     <img style={{
                         maxHeight: '90vh',
+                        objectFit: 'contain'
                     }}
                         className='d-block w-100'
                         src={Basement1}
@@ -21,11 +25,22 @@ const Slider = () => {
                 <Carousel.Item interval={5000}>
                     <img style={{
                         maxHeight: '90vh',
+                        objectFit: 'contain'
                     }}
                         className='d-block w-100'
                         src={Basement2}
                         alt='second slide' />
                 </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img style={{
+                        maxHeight: '90vh',
+                        objectFit: 'contain'
+                    }}
+                        className='d-block w-100'
+                        src={F2}
+                        alt='third slide' />
+                </Carousel.Item>
+            
             </Carousel>
         </div>
     );
