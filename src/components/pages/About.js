f2a55893716fcaa52014a6b003e37691f74adc23
Styrlingjohn <styrling_morris@yahoo.com>
2024-01-03 16:19:09 -0700

    added some styling to about the company

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,13 +3,24 @@ import React from 'react';
 
 
 
+
 function About() {
 
   return (
-    <div>
-      About the company
+    <div className=' A-C container-fluid'>
+
+      <div className='A-C-1 container'>
+        Kadin Headshot here
+      </div>
+
+
+      <div className='A-C-1 container'>
+        About the Company here
+      </div>
+
+
     </div>
-    
+
   );
 }
 
