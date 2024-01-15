939c5283935bd480899d94c003921138ed459f34
Styrlingjohn <styrling_morris@yahoo.com>
2024-01-15 08:28:02 -0700

    added value boxes to about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,25 +7,77 @@ import React from 'react';
 function About() {
 
   return (
-    <div className=' A-C container-fluid'>
+    <div className=' about-all container-fluid'>
+      <div className=' A-C container-fluid'>
+        <div className='A-C-1 container'>
+          <img
+            src='./Pics/Headshot.jpg'
+            alt='headshot'
+            style={{ width: '50%', height: '100%' }}></img>
+        </div>
 
-      <div className='A-C-1 container'>
-        <img src='./Pics/Headshot.jpg' alt='headshot' style={{ width: '50%', height: '100%' }}></img>
-      </div>
 
-
-      <div className='A-C-1 container'>
-        <h1>About KT Nokes Custom Homes</h1>
-        <div className='Atext'>
-          At KT Nokes Custom Homes, we're dedicated to bringing your construction dreams to life. With a
-          commitment to quality and client satisfaction, our experienced team handles residential and commercial
-          projects, ensuring precision and reliability in every aspect of construction. From renovations and
-          basements to new builds, we focus on delivering excellence while prioritizing open communication and
-          transparency throughout the process. With over 8 years of experience in new construction, trust KT Nokes
-          Custom Homes for your construction needs and let's build your dreams together.
+        <div className='A-C-1 container'>
+          <h1>About KT Nokes Custom Homes</h1>
+          <div className='Atext'>
+            At KT Nokes Custom Homes, we're dedicated to bringing your construction dreams to life. With a
+            commitment to quality and client satisfaction, our experienced team handles residential and commercial
+            projects, ensuring precision and reliability in every aspect of construction. From renovations and
+            basements to new builds, we focus on delivering excellence while prioritizing open communication and
+            transparency throughout the process. With over 8 years of experience in new construction, trust KT Nokes
+            Custom Homes for your construction needs and let's build your dreams together.
+          </div>
         </div>
       </div>
 
+
+
+      <div className='A-C-2'>
+        <h1 style={{ color: 'white' }}>Our Core Values</h1>
+        <div className='core'>
+          <div className='core-H'>
+            <div className='C-text'>
+              <h5>Quality</h5>
+              We take immense pride in our craftsmanship, ensuring that each project reflects
+              the highest standards of quality and attention to detail.
+            </div>
+            <div className='c-Img-H rounded-circle bg-white'>
+              <img
+                style={{ width: '100%' }}
+                src='./Pics/Icons/quality.png'
+                alt='quality'
+              />
+            </div>
+          </div>
+          <div className='core-H'>
+            <div className='C-text'>
+              <h5>Open Communication</h5>
+              We conduct business ethically, valuing transparency, fairness, and integrity in all our interactions.
+            </div>
+            <div className='c-Img-H rounded-circle bg-white'>
+              <img
+                style={{ width: '100%' }}
+                src='./Pics/Icons/communication.png'
+                alt='quality'
+              />
+            </div>
+          </div>
+          <div className='core-H'>
+            <div className='C-text'>
+              <h5>Customer satisfaction</h5>
+              We excel by understanding needs, on-time and attentive feedback. Our aim is lasting relationships through
+              exceptional service, setting us apart in construction.
+            </div>
+            <div className='c-Img-H rounded-circle bg-white'>
+              <img
+                style={{ width: '100%' }}
+                src='./Pics/Icons/customer.png'
+                alt='quality'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
 
     </div>
 
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,68 +5,69 @@ import React from 'react';
 function Contact() {
 
     return (
-        <div className='container mt-5' style={{color:'white'}}>
-            <h1> Contact me! </h1>
+        <div className='contact-H'>
+            <div className='container mt-5' style={{ color: 'white', paddingTop: '5%', paddingBottom: '5%' }}>
+                <div style={{ textAlign: 'center' }}>
+                    <h1> Contact me! </h1>
+                    <span > *Basements must be at minimum of 500 Square Feet </span>
+                </div>
+                <br />
 
-            <div>
-                <span className='F-Span'> *Basements must be at minimum of 500 Square Feet </span>
+                <form className='row g-3' action="https://formsubmit.co/ktnokes@gmail.com" method="POST">
+                    <div className='col-md-6'>
+                        <label for='firstName' className='form-label'> First Name: </label>
+                        <input type='text' className='form-control' name='name' id="firstName"
+                            placeholder='First Name' required></input>
+                    </div>
+                    <div className='col-md-6'>
+                        <label for='lastName' className='form-label'> Last Name: </label>
+                        <input type='text' className='form-control' name='Last Name' id="lastName"
+                            placeholder='Last Name' required></input>
+                    </div>
+                    <div className='col-md-8'>
+                        <label for="emailInfo" className='form-label'> E-Mail: </label>
+                        <input type='email' name='Email' className='form-control' id='emailInfo'
+                            placeholder='JohnDoe@email.com' required></input>
+                    </div>
+                    <div className='col-md-4'>
+                        <label for='phoneNumber' className='form-label'> Phone Number: </label>
+                        <input type='text' className='form-control' name='Phone Number' id='phoneNumber'
+                            required placeholder='123-456-7899'></input>
+                    </div>
+
+                    <div className='col-md-4'>
+                        <label for='projectType' className='form-label'>Project Type: </label>
+                        <select type='select' className='form-control' name='Project Type' id='projectType' required>
+                            <option> ~Select Here~ </option>
+                            <option> Basement Finish</option>
+                            <option> Remodel </option>
+                            <option> New build </option>
+                            <option> Addition </option>
+                            <option> Other </option>
+                        </select>
+                    </div>
+                    <div className='col-md-8'>
+                        <label for='projectLocation' className='form-label'> Project Location: </label>
+                        <input type='text' className='form-control' name='Project Location' id='projectLocation'
+                            required placeholder='City'></input>
+                    </div>
+                    <div className='col-md-4'>
+                        <label for='sqFootage' className='form-label'> Approx. Sq. Footage (min of 500 sqft) </label>
+                        <input type='text' className='form-control' name='Sq Footage' id='sqFootage'
+                            required placeholder='500*'></input>
+                    </div>
+
+                    <div className='col-md-12'>
+                        <label for="comments" className='form-label'> Description of Project </label>
+                        <textarea className='form-control' id='comments' name='comments' rows='7' required></textarea>
+                    </div>
+
+                    <div className='col-md-12'>
+                        <button type='submit' className='btn btn-primary'> Submit </button>
+                    </div>
+                </form>
+                <br />
             </div>
-            <br/>
-
-            <form className='row g-3' action="https://formsubmit.co/ktnokes@gmail.com" method="POST">
-                <div className='col-md-6'>
-                    <label for='firstName' className='form-label'> First Name: </label>
-                    <input type='text' className='form-control' name='name' id="firstName"
-                        placeholder='First Name' required></input>
-                </div>
-                <div className='col-md-6'>
-                    <label for='lastName' className='form-label'> Last Name: </label>
-                    <input type='text' className='form-control' name='Last Name' id="lastName"
-                        placeholder='Last Name' required></input>
-                </div>
-                <div className='col-md-8'>
-                    <label for="emailInfo" className='form-label'> E-Mail: </label>
-                    <input type='email' name='Email' className='form-control' id='emailInfo'
-                        placeholder='JohnDoe@email.com' required></input>
-                </div>
-                <div className='col-md-4'>
-                    <label for='phoneNumber' className='form-label'> Phone Number: </label>
-                    <input type='text' className='form-control' name='Phone Number' id='phoneNumber'
-                        required placeholder='123-456-7899'></input>
-                </div>
-
-                <div className='col-md-4'>
-                    <label for='projectType' className='form-label'>Project Type: </label>
-                    <select type='select' className='form-control' name='Project Type' id='projectType' required>
-                        <option> ~Select Here~ </option>
-                        <option> Basement Finish</option>
-                        <option> Remodel </option>
-                        <option> New build </option>
-                        <option> Addition </option>
-                        <option> Other </option>
-                    </select>
-                </div>
-                <div className='col-md-8'>
-                    <label for='projectLocation' className='form-label'> Project Location: </label>
-                    <input type='text' className='form-control' name='Project Location' id='projectLocation'
-                        required placeholder='City'></input>
-                </div>
-                <div className='col-md-4'>
-                    <label for='sqFootage' className='form-label'> Approx. Sq. Footage (min of 500 sqft) </label>
-                    <input type='text' className='form-control' name='Sq Footage' id='sqFootage'
-                        required placeholder='500*'></input>
-                </div>
-
-                <div className='col-md-12'>
-                    <label for="comments" className='form-label'> Description of Project </label>
-                    <textarea className='form-control' id='comments' name='comments' rows='7' required></textarea>
-                </div>
-
-                <div className='col-md-12'>
-                    <button type='submit' className='btn btn-primary'> Submit </button>
-                </div>
-            </form>
-            <br/>
         </div>
     );
 }
